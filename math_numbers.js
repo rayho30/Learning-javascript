b858b0ae98fa1852d68a404e4cceb1045191a75b
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-11-07 21:32:44 +0600

    random number

---
+++ math_numbers.js
@@ -0,0 +1,12 @@
+let randomNum = Math.floor(Math.random() * 10) + 1;
+console.log(randomNum);
+
+let a = 15;
+let b = 4;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+console.log("Power:", a ** b);
