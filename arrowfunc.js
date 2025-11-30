257419692d937a83e1bf0de2edbd0c0f1a46e60f
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-12-01 01:00:45 +0600

    Add files via upload

---
+++ arrowfunc.js
@@ -0,0 +1,10 @@
+const addtwo = (num1,num2) => {
+    return num1+num2;
+
+}
+console.log(addtwo(5,4))
+
+//implict return
+const addTwo = (num1, num2) => num1 + num2;
+
+console.log(addTwo(5, 5)); 
